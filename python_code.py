8400fd17fe0c4d7f486bb957ad7d84e9d79fc0a3
Scott West <west55721@gmail.com>
2025-08-01 16:48:29 +1000

    added a basic python calculation

--- python_code.py
+++ python_code.py
@@ -1,1 +1,9 @@
 print('Hello World!')
+
+def calculate_sum(a, b):
+    return a + b
+
+calculation = calculate_sum(10,20)
+
+print(f'The sum of 10 and 20 is: {calculation}')
+print('This is a simple Python script.')
